2d5608d610f73c87ebb821ce6bcb3c335084ab8c
Shriram <shrirammadurai2007@gmail.com>
2026-08-06 14:46:42 +0530

    Add infix to postfix conversion function
    Implement infix to postfix conversion with precedence handling.

---
+++ 6a.py
@@ -0,0 +1,37 @@
+def precedence(op):
+    if op == '+' or op == '-':
+        return 1
+    elif op == '*' or op == '/':
+        return 2
+    elif op == '^':
+        return 3
+    return 0
+
+def infix_to_postfix(expression):
+    stack = []
+    postfix = ""
+
+    for ch in expression:
+        if ch.isalnum():
+            postfix += ch
+
+        elif ch == '(':
+            stack.append(ch)
+
+        elif ch == ')':
+            while stack and stack[-1] != '(':
+                postfix += stack.pop()
+            stack.pop()
+
+        else:
+            while stack and stack[-1] != '(' and precedence(stack[-1]) >= precedence(ch):
+                postfix += stack.pop()
+            stack.append(ch)
+
+    while stack:
+        postfix += stack.pop()
+
+    return postfix
+
+expr = input("Enter Infix Expression: ")
+print("Postfix Expression:", infix_to_postfix(expr))
